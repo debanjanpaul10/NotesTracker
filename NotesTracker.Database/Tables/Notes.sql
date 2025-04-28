3be6a8b2a98608fcb822dbe4318ddbf420bfda5b
Debanjan Paul <debanjanpaul10@gmail.com>
2025-04-29 00:38:52 +0530

    Updated indentation

--- NotesTracker.Database/Tables/Notes.sql
+++ NotesTracker.Database/Tables/Notes.sql
@@ -1,11 +1,12 @@
-CREATE TABLE [dbo].[Notes](
-	[NoteId] [int] IDENTITY(1,1) NOT NULL,
-	[NoteTitle] [nvarchar](max) NOT NULL,
-	[NoteDescription] [nvarchar](max) NOT NULL,
-	[CreatedDate] [datetime] NOT NULL,
-	[LastModifiedDate] [datetime] NOT NULL,
-	[IsActive] [bit] NOT NULL,
- CONSTRAINT [PK_Notes] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Notes]
+(
+  [NoteId] [int] IDENTITY(1,1) NOT NULL,
+  [NoteTitle] [nvarchar](max) NOT NULL,
+  [NoteDescription] [nvarchar](max) NOT NULL,
+  [CreatedDate] [datetime] NOT NULL,
+  [LastModifiedDate] [datetime] NOT NULL,
+  [IsActive] [bit] NOT NULL,
+  CONSTRAINT [PK_Notes] PRIMARY KEY CLUSTERED 
 (
 	[NoteId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
--- NotesTracker.Database/Tables/Users.sql
+++ NotesTracker.Database/Tables/Users.sql
@@ -1,10 +1,11 @@
-CREATE TABLE [dbo].[Users](
-	[UserId] [int] IDENTITY(1,1) NOT NULL,
-	[UserEmail] [nvarchar](max) NOT NULL,
-	[UserName] [nvarchar](max) NOT NULL,
-	[UserPassword] [nvarchar](max) NOT NULL,
-	[IsActive] [bit] NOT NULL,
- CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Users]
+(
+  [UserId] [int] IDENTITY(1,1) NOT NULL,
+  [UserEmail] [nvarchar](max) NOT NULL,
+  [UserName] [nvarchar](max) NOT NULL,
+  [UserPassword] [nvarchar](max) NOT NULL,
+  [IsActive] [bit] NOT NULL,
+  CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
 (
 	[UserId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]

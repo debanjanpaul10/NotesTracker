f22a920843927e40968657d348d9cfde40ea8d72
Debanjan Paul <debanjanpaul10@gmail.com>
2025-05-03 00:58:57 +0530

    Added Azure App Config + Azure DB

--- NotesTracker.API/Middleware/ConfigureCoreServices.cs
+++ NotesTracker.API/Middleware/ConfigureCoreServices.cs
@@ -8,7 +8,9 @@
 namespace NotesTracker.API.Middleware
 {
     using System.Security.Claims;
+    using Azure.Identity;
     using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.Extensions.Configuration.AzureAppConfiguration;
     using NotesTracker.API.Controllers;
     using NotesTracker.Shared.Constants;
     using static NotesTracker.Shared.Constants.ConfigurationConstants;
@@ -19,23 +21,48 @@ namespace NotesTracker.API.Middleware
     public static class ConfigureCoreServices
     {
         /// <summary>
+        /// Configures azure app configuration.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="credentials">The credentials.</param>
+        /// <exception cref="InvalidOperationException">InvalidOperationException error.</exception>
+        public static void ConfigureAzureAppConfiguration(this WebApplicationBuilder builder, DefaultAzureCredential credentials)
+        {
+            var appConfigurationEndpoint = builder.Configuration[AppConfigurationEndpointKeyConstant];
+            if (string.IsNullOrEmpty(appConfigurationEndpoint))
+            {
+                throw new InvalidOperationException(ExceptionConstants.MissingConfigurationMessage);
+            }
+
+            builder.Configuration.AddAzureAppConfiguration(options =>
+            {
+                options.Connect(new Uri(appConfigurationEndpoint), credentials)
+                    .Select(KeyFilter.Any).Select(KeyFilter.Any, BaseConfigurationAppConfigKeyConstant)
+                    .Select(KeyFilter.Any, NotesAPIAppConfigKeyConstant)
+                    .ConfigureKeyVault(configure =>
+                    {
+                        configure.SetCredential(credentials);
+                    });
+            });
+        }
+
+        /// <summary>
         /// Configures api services.
         /// </summary>
         /// <param name="builder">The builder.</param>
         public static void ConfigureApiServices(this WebApplicationBuilder builder)
         {
-            builder.Services.ConfigureServices();
             builder.ConfigureApplicationDependencies();
+            builder.ConfigureAuthenticationServices();
             builder.Services.ConfigureBusinessDependencies();
             builder.Services.ConfigureDataDependencies();
-            builder.ConfigureAuthenticationServices();
         }
 
         /// <summary>
-		/// Configures authentication services.
-		/// </summary>
-		/// <param name="builder">The builder.</param>
-		public static void ConfigureAuthenticationServices(this WebApplicationBuilder builder)
+        /// Configures authentication services.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        public static void ConfigureAuthenticationServices(this WebApplicationBuilder builder)
         {
             var configuration = builder.Configuration;
             builder.Services.AddAuthentication(options =>

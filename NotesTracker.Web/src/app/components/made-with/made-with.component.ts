45aff2a9f5e4089a32b781aa280a5293c6f634a8
Debanjan Paul <debanjanpaul10@gmail.com>
2025-05-08 01:07:10 +0530

    Added carousel

--- NotesTracker.Web/src/app/components/made-with/made-with.component.ts
+++ NotesTracker.Web/src/app/components/made-with/made-with.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 
 import { MadeWithComponentConstants } from '../../helpers/notestracker.constants';
 
@@ -12,7 +13,13 @@ import { MadeWithComponentConstants } from '../../helpers/notestracker.constants
 @Component({
   selector: 'app-made-with',
   standalone: true,
-  imports: [MatCardModule, MatChipsModule, CommonModule, MatButtonModule],
+  imports: [
+    MatCardModule,
+    MatChipsModule,
+    CommonModule,
+    MatButtonModule,
+    MdbCarouselModule,
+  ],
   templateUrl: './made-with.component.html',
   styleUrl: './made-with.component.scss',
 })
@@ -20,7 +27,7 @@ class MadeWithComponent {
   /**
    * The MWC Data
    */
-  public mwcObjects = signal(MadeWithComponentConstants);
+  public mwcObjects = MadeWithComponentConstants;
 
   /**
    * Handles link redirection

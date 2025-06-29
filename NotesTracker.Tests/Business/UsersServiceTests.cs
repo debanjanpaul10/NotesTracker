4d291bf7ce4f5cb40587f9eaadb32550b8ec3f73
Debanjan Paul <debanjanpaul10@gmail.com>
2025-06-29 22:59:10 +0530

    Added unit tests stage#1 -> Notes + Users

---
+++ NotesTracker.Tests/Business/UsersServiceTests.cs
@@ -0,0 +1,57 @@
+﻿// *********************************************************************************
+//	<copyright file="UsersServiceTests.cs" company="Personal">
+//		Copyright (c) 2025 Personal
+//	</copyright>
+// <summary>Users Service Tests Class.</summary>
+// *********************************************************************************
+
+namespace NotesTracker.Tests.Business
+{
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+	using Moq;
+	using NotesTracker.Data.Contracts;
+	using Xunit;
+	using NotesTracker.Business.Services;
+
+	/// <summary>
+	/// Users Service Tests Class.
+	/// </summary>
+	public class UsersServiceTests
+	{
+		/// <summary>
+		/// The mock users data service
+		/// </summary>
+		private readonly Mock<IUsersDataService> _mockUsersDataService;
+
+		/// <summary>
+		/// The user service
+		/// </summary>
+		private readonly UsersService _userService;
+
+		public UsersServiceTests()
+		{
+			this._mockUsersDataService = new Mock<IUsersDataService>();
+			this._userService = new UsersService(this._mockUsersDataService.Object);
+		}
+
+		/// <summary>
+		/// Adds the users asynchronous maps and calls data service with correct users.
+		/// </summary>
+		[Fact]
+		public async Task AddUsersAsync_MapsAndCallsDataService_WithCorrectUsers()
+		{
+			// Arrange
+			var mockUsersData = TestsHelper.PrepareUsersDataListDTO();
+			this._mockUsersDataService.Setup(x => x.AddUsersAsync(It.IsAny<List<Data.Entities.User>>()));
+
+			// Act
+			await this._userService.AddUsersAsync(mockUsersData);
+
+			// Assert
+			this._mockUsersDataService.Verify(x => x.AddUsersAsync(It.IsAny<List<Data.Entities.User>>()), Times.Once);
+		}
+
+		
+	}
+}
---
+++ NotesTracker.Tests/TestsHelper.cs
@@ -0,0 +1,92 @@
+﻿// *********************************************************************************
+//	<copyright file="TestsHelper.cs" company="Personal">
+//		Copyright (c) 2025 Personal
+//	</copyright>
+// <summary>The Tests Helper Class.</summary>
+// *********************************************************************************
+
+namespace NotesTracker.Tests
+{
+	using NotesTracker.Data.Entities;
+	using NotesTracker.Shared.DTO;
+
+	/// <summary>
+	/// The Tests Helper Class.
+	/// </summary>
+	static class TestsHelper
+	{
+		public static UsersDataDTO PrepareUsersDataDTO(int userNumber)
+		{
+			return new UsersDataDTO
+			{
+				Email = "user@mail.com",
+				Identities = [
+					new IdentityDto()
+					{
+						IsSocial = false,
+						Provider = "Microsoft",
+						UserId = Guid.NewGuid().ToString(),
+					},
+					new IdentityDto()
+					{
+						IsSocial = false,
+						Provider = "Google",
+						UserId = Guid.NewGuid().ToString(),
+					},
+					new IdentityDto()
+					{
+						IsSocial = false,
+						Provider = "Facebook",
+						UserId = Guid.NewGuid().ToString(),
+					}
+
+				],
+				UserName = $"Sample User {userNumber}",
+			};
+		}
+
+		/// <summary>
+		/// Prepares the users data list dto.
+		/// </summary>
+		/// <returns>The list of <see cref="UsersDataDTO"/></returns>
+		public static List<UsersDataDTO> PrepareUsersDataListDTO()
+		{
+			return new List<UsersDataDTO>()
+			{
+				PrepareUsersDataDTO(1),
+				PrepareUsersDataDTO(2),
+				PrepareUsersDataDTO(3)
+			};
+		}
+
+		/// <summary>
+		/// Prepares the note dto.
+		/// </summary>
+		/// <param name="userName">Name of the user.</param>
+		/// <returns>The note dto.</returns>
+		public static NoteDTO PrepareNoteDTO(string userName)
+		{
+			return new NoteDTO { NoteTitle = "Test", NoteDescription = "Desc", UserName = userName };
+		}
+
+		/// <summary>
+		/// Prepares the notes data.
+		/// </summary>
+		/// <param name="userName">Name of the user.</param>
+		/// <returns>The list of <see cref="Note"/></returns>
+		public static List<Note> PrepareNotesData(string userName)
+		{
+			return [new Note { NoteId = 1, NoteTitle = "A", UserName = userName }];
+		}
+
+		/// <summary>
+		/// Prepares the update note dto.
+		/// </summary>
+		/// <param name="userName">Name of the user.</param>
+		/// <returns>The update note dto.</returns>
+		public static UpdateNoteDTO PrepareUpdateNoteDTO(string userName)
+		{
+			return new UpdateNoteDTO { NoteId = 1, NoteTitle = "Updated", NoteDescription = "Desc", UserName = userName };
+		}
+	}
+}
